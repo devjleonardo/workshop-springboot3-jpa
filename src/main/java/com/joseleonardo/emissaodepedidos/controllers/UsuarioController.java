3690e4c351c0beeb9e4a71462bc61e0aff7af3d4
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-27 20:55:39 -0300

    Camada de serviço e registro de componentes

--- src/main/java/com/joseleonardo/emissaodepedidos/controllers/UsuarioController.java
+++ src/main/java/com/joseleonardo/emissaodepedidos/controllers/UsuarioController.java
@@ -1,20 +1,35 @@
 package com.joseleonardo.emissaodepedidos.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joseleonardo.emissaodepedidos.domain.entities.Usuario;
+import com.joseleonardo.emissaodepedidos.domain.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> listar() {
-		Usuario usuario = new Usuario(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+	public ResponseEntity<List<Usuario>> listar() {
+		List<Usuario> usuarios = usuarioService.listar();
 		
+		return ResponseEntity.ok().body(usuarios);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> buscar(@PathVariable Long id) {
+		Usuario usuario = usuarioService.buscarPorId(id);
+
 		return ResponseEntity.ok().body(usuario);
 	}
 	
---
+++ src/main/java/com/joseleonardo/emissaodepedidos/domain/services/UsuarioService.java
@@ -0,0 +1,28 @@
+package com.joseleonardo.emissaodepedidos.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joseleonardo.emissaodepedidos.domain.entities.Usuario;
+import com.joseleonardo.emissaodepedidos.domain.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> listar() {
+		return usuarioRepository.findAll();
+	}
+	
+	public Usuario buscarPorId(Long id) {
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return usuario.get();
+	}
+	
+}
