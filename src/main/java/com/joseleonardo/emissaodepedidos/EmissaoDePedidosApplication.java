151c8bf66c98fab87a2c46d143cea28ba8e3c598
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-27 20:36:19 -0300

    Criação do projeto

---
+++ src/main/java/com/joseleonardo/emissaodepedidos/EmissaoDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.joseleonardo.emissaodepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissaoDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissaoDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseleonardo/emissaodepedidos/EmissaoDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseleonardo.emissaodepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissaoDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
