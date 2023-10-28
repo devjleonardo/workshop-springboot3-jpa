62080a2086b8357d972713aaa469e113f70d56e9
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-27 21:12:47 -0300

    Associação muitos-para-muitos com JoinTable

--- src/main/java/com/joseleonardo/emissaodepedidos/config/TestConfig.java
+++ src/main/java/com/joseleonardo/emissaodepedidos/config/TestConfig.java
@@ -49,6 +49,15 @@ public class TestConfig implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5));
 		
+		produto1.addCategoria(categoria2);
+		produto2.addCategoria(categoria1);
+		produto2.addCategoria(categoria3);
+		produto3.addCategoria(categoria3);
+		produto4.addCategoria(categoria3);
+		produto5.addCategoria(categoria2);
+
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5));
+		
 		Usuario usuario1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		Usuario usuario2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
--- src/main/java/com/joseleonardo/emissaodepedidos/domain/entities/Produto.java
+++ src/main/java/com/joseleonardo/emissaodepedidos/domain/entities/Produto.java
@@ -2,14 +2,17 @@ package com.joseleonardo.emissaodepedidos.domain.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_produto")
@@ -24,8 +27,11 @@ public class Produto implements Serializable {
 	private String descricao;
 	private Double preco;
 	private String imgUrl;
-
-	@Transient
+	
+	@ManyToMany
+	@JoinTable(name = "tb_produto_ categoria",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private Set<Categoria> categorias = new HashSet<>();
 
 	public Produto() {
@@ -83,5 +89,26 @@ public class Produto implements Serializable {
 	public Set<Categoria> getCategorias() {
 		return categorias;
 	}
+	
+	public boolean addCategoria(Categoria Categoria) {
+		return getCategorias().add(Categoria);
+	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 }
